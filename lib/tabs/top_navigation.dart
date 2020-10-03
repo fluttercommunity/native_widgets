96a1686dff5f5346780a456bb5f31fd00d1c584e
Mohammad Fayaz <fayazfz07@gmail.com>
2020-10-03 19:53:16 +0530

    fixes unmodifiable Map error, adds TabController to NativeTopTabs for Material

--- lib/tabs/top_navigation.dart
+++ lib/tabs/top_navigation.dart
@@ -6,6 +6,7 @@ class NativeTopTabs extends StatelessWidget {
   final ValueChanged<dynamic> onValueChanged;
   final dynamic groupValue;
   final List<Widget> tabs;
+  final TabController tabController;
 
   NativeTopTabs({
     this.showMaterial = false,
@@ -15,18 +16,19 @@ class NativeTopTabs extends StatelessWidget {
     this.onValueChanged,
     this.tabs,
     this.groupValue,
+    this.tabController,
   });
 
   @override
   Widget build(BuildContext context) {
     final bool _isIos = showCupertino(showMaterial: showMaterial);
-    final Map<int, Widget> iosTabs = const <int, Widget>{};
+    Map<int, Widget> iosTabs = <int, Widget>{};
 
     int _index = 0;
     for (Widget item in tabs) {
       iosTabs[_index] = item;
+      _index++;
     }
-    _index++;
 
     if (_isIos) {
       return CupertinoSegmentedControl<dynamic>(
@@ -39,8 +41,22 @@ class NativeTopTabs extends StatelessWidget {
       );
     }
 
-    return TabBar(
-      tabs: tabs,
+    if (tabController != null) {
+      return TabBar(
+        controller: tabController,
+        tabs: tabs,
+        labelColor: selectedColor,
+        onTap: onValueChanged,
+      );
+    }
+    
+    return DefaultTabController(
+      length: tabs.length,
+      child: TabBar(
+        tabs: tabs,
+        labelColor: selectedColor,
+        onTap: onValueChanged,
+      ),
     );
   }
 }
